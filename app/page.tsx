fb148b8349a34489b48031a00f741889be107894
fernandoberbel <fberbel1000@gmail.com>
2024-11-06 02:15:18 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
-  return <h1 className="text-red-700">Hello World</h1>;
+  return <Button>Hello World</Button>;
 }
